47a22418bc2f23a7fd5daa7b90453aae1cf4ce56
EndikaPM <endika.perez.mas@gmail.com>
2026-05-05 07:55:28 +0200

    hide files in invoice

---
+++ tpv_plus_hide_files/README.md
@@ -0,0 +1,7 @@
+# tpv-plus-hide-files
+
+Modulo para Odoo 18 que hereda `account.report_invoice_document` y oculta en la impresion de factura:
+
+- Fecha de factura (`invoice_date`).
+- Identificador principal (`o.name`).
+
---
+++ tpv_plus_hide_files/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "tpv-plus-hide-files",
+    "version": "18.0.1.0.0",
+    "summary": "Oculta fecha e identificador en la impresion de factura",
+    "category": "Accounting/Accounting",
+    "author": "Xtendoo",
+    "license": "LGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/report_invoice.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
+
